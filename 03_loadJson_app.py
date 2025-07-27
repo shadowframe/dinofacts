d575e429a6bdaf0b7d08a923344428e3cdfb195e
Jan <jan.weber123@googlemail.com>
2025-07-28 01:07:02 +0200

    did sort the content of the dino.js by asc, id

---
+++ 03_loadJson_app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import time
+import json
+
+funfact = """
+:rainbow[*Filtere*] :orange[*Namen*] | :green[*Arten*] | :violet[*Fortbewegungsarten*]
+"""
+
+def stream_data():
+    for word in funfact.split(" "):
+        yield word + " "
+        time.sleep(0.30)
+
+# Load the dino.json file
+def load_dino_data():
+    with open("data/dino.json", "r") as file:  # Adjust the path if necessary
+        return json.load(file)
+
+st.title("dinoFacts 🦖")
+st.write_stream(stream_data)
+
+# Display the loaded JSON data
+dino_data = load_dino_data()
+st.json(dino_data)  # Use Streamlit's st.json to display JSON data nicely
+
--- README.md
+++ README.md
@@ -50,7 +50,11 @@ Standardmäßig läuft die Anwendung auf Port 8501. Wenn du einen anderen Port n
 
 `streamlit run 01_start_app.py --server.port=80`
 
-## Installation 02_fancyText_app.py
+## todo 02_fancyText_app.py
+
+## loading 03_loadJson_app.py
+
+https://www.w3schools.com/python/python_json.asp
 
 
 

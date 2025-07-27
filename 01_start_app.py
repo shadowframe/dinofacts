87a7e3a3c7fc2275b97bb7a00ee2f4cd8c1fc014
Jan <jan.weber123@googlemail.com>
2025-07-27 23:03:33 +0200

    ...

--- 01_start_app.py
+++ 01_start_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 
-st.title("Hallo du! 👋")
+st.title("dinoFacts 🦖")
 st.markdown("""
-Willkommen zu meiner App! Hier kannst du verschiedene Funktionen ausprobieren.
-- **Seite 1**: Einführung
-- **Seite 2**: Datenanalyse
-- **Seite 3**: Visualisierungen
+Willkommen zu meiner App! Hier kannst du Dinos:
+- **Seite 1**: nach Namen suchen
+- **Seite 2**: nach Arten filtern
 """) 

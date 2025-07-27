eb98e5e4824a1959b182189efbbfc65465a7b749
Jan <jan.weber123@googlemail.com>
2025-07-27 22:58:47 +0200

    kleiner committest

--- 01_start_app.py
+++ 01_start_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Hallo du! 👋")
+st.markdown("""
+Willkommen zu meiner App! Hier kannst du verschiedene Funktionen ausprobieren.
+- **Seite 1**: Einführung
+- **Seite 2**: Datenanalyse
+- **Seite 3**: Visualisierungen
+""") 

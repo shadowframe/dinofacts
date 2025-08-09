7ca3d785afd98b516f65c7a5a71f72666950a307
Jan <jan.weber123@googlemail.com>
2025-08-09 16:47:38 +0200

    first commit

---
+++ 05_diet.py
@@ -0,0 +1,82 @@
+import streamlit as st
+import time
+import json
+
+funfact = """
+:rainbow[*Filtere*] :orange[*Namen*] | :green[*Arten*] | :violet[*Fortbewegungsarten*]
+"""
+
+### Functions ###
+print("Variable funfact wird an dieser Stelle ausgegeben")
+def stream_data():
+    for word in funfact.split(" "):
+        yield word + " "
+        time.sleep(0.30)
+
+# Load the dino_names.json file and parse it into a Python dictionary
+def load_dino_data():
+    with open("data/dino_names.json", "r") as file:  # Adjust the path if necessary
+        return json.load(file)
+
+def load_all_dino_data():
+    with open("data/dino.json", "r") as file:  # Adjust the path if necessary
+        return json.load(file)
+    
+def get_diet_by_id(dino_data, dino_id):
+    # Search for the dinosaur with the matching ID
+    for dino in dino_data:
+        if dino.get("id") == dino_id:
+            return dino.get("diet", "Diet information not available")
+    return "Dinosaur with the given ID not found"
+
+def get_diet_image_by_diet(diet):
+    if diet == "herbivore":
+        return "https://example.com/herbivore_image.png"
+    if diet == "carnivore":
+        return "https://example.com/carnivore_image.png"
+    if diet == "omnivore":
+        return "https://example.com/omnivore_image.png"
+    return "https://example.com/default_image.png"
+
+
+
+### Functions ###
+# fancy_text returns a stream of text from the stream_data function
+@st.fragment
+def fancy_text():
+    st.write_stream(stream_data)
+
+# search creates a search bar and displays the selected dinosaur's name and ID
+@st.fragment
+def search():
+    # Autocomplete search
+    st.markdown("### **Search for a Dinosaur:**")
+
+    # Create a list of dinosaur names for selection
+    dino_names = [dino['name'] for dino in dino_data]
+
+    # Add a selectbox for dinosaur selection
+    selected_dino_name = st.selectbox("Select a Dinosaur:", dino_names)
+
+    # Find the corresponding ID for the selected dinosaur
+    selected_dino_id = next((dino['id'] for dino in dino_data if dino['name'] == selected_dino_name), None)
+
+    # Display the selected dinosaur's name and ID
+    if selected_dino_name:
+        st.write(f"Selected Dinosaur Name: {selected_dino_name}")
+        st.write(f"Selected Dinosaur ID: {selected_dino_id}")
+        # Load the data
+        all_dino_data = load_all_dino_data()
+        dino_id = selected_dino_id
+        diet = get_diet_by_id(all_dino_data, dino_id)
+        st.write(f"The diet for dinosaur with ID {dino_id} is: {diet}")
+
+# Load the data of the dinosaurs names and id´s
+dino_data = load_dino_data()
+
+### GUI ###
+st.title("dinoFacts 🦖")
+
+fancy_text()
+
+search()

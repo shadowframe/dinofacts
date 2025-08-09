a81d47aefff3784880e20556c273b5f9ebda5a4c
Jan <jan.weber123@googlemail.com>
2025-08-09 15:09:44 +0200

    Add turtle graphics drawing scripts: star with pauses and simple forward-right sequence

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import turtle
+import time
 
+box = turtle.Turtle()
 star = turtle.Turtle()
 
 star.right(75)
@@ -8,5 +10,23 @@ star.forward(100)
 for i in range(4):
     star.right(144)
     star.forward(100)
+
+box.forward(100)
+time.sleep(5)
+
+box.right(90)
+box.forward(100)
+time.sleep(5)
+
+box.right(90)
+box.forward(200)
+time.sleep(5)
+
+box.right(90)
+box.forward(100)
+time.sleep(5)
+
+box.right(90)
+box.forward(100)
 
 turtle.done()
---
+++ test2.py
@@ -0,0 +1,7 @@
+import turtle
+
+turtle.forward(50)
+turtle.right(90) #Die Turtle dreht sich um den angegebenen
+turtle.forward(50)
+
+turtle.done()
